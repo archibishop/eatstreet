df3f2f4b1193493849b21d3e6c865a0f1877f61f
archibishop <wagubib@gmail.com>
2018-04-27 01:42:31 -0700

    javascript for POSTS

---
+++ js/register.js
@@ -0,0 +1,59 @@
+
+data = {
+    "email":"brian@gmail.com",
+    "password":"12345",
+    "firstName":"craig",
+    "lastName":"red",
+    "phone":"074893746"
+}
+
+$(document).ready(function(){
+    submitRegister(data)
+});
+
+
+
+function submitRegister(data) {
+                $.ajax({
+                    headers: {"X-ACCESS-Token" : "f716ae617b07023d"},
+                    url: 'https://api.eatstreet.com/publicapi/v1/register-user',
+                    method: 'POST',
+                    data: JSON.stringify(data),// now data come in this function
+                    contentType: "application/json",
+                    dataType: "json",
+                    // data: $("#register-form").serialize(),
+                    success: function (rawData) {
+                        // var parsed = JSON.parse(rawData);
+                        var parsed = JSON.stringify(rawData);
+
+                        // if (!parsed) return;
+                        // var searchResults = $("#search-results");
+                        // print(rawData)
+                        // alert("" + rawData)
+                        alert('success')
+                        alert(rawData.apiKey)
+                        alert(rawData.firstName)
+                        alert(rawData.lastName)
+                        alert(rawData.phone)
+                        alert(rawData.email)
+                        // searchResults.empty();
+
+                        // parsed.forEach(function (result) {
+                        //     var row = $("<tr><td>" + result.Title + "</td><td>" + result.Author +
+                        //         "</td><td>" + result.Year + "</td><td>" + result.ID +
+                        //         "</td></tr>");
+                        //     searchResults.append(row);
+                             // row.on("click", function () {
+                        //         $.ajax({
+                        //             url: "/books?id=" + result.ID,
+                        //             method: "PUT",
+                        //             success: function (data) {
+                                        
+                        //             }
+                        //         })
+                         //    })
+                         // });
+                    }
+                });
+                return false;
+            }

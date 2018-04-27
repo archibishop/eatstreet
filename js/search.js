1bb85f273e4f38ca1d6133a657d8620d516c1f13
Nsubuga Solomon <snsubuga@live.com>
2018-04-27 11:46:23 +0300

    feat: Add search restaurants api call

---
+++ js/search.js
@@ -0,0 +1,5 @@
+// var url =
+//   "https://api.eatstreet.com/publicapi/v1/restaurant/search?method=both&street-address=316+W.+Washington+Ave.+Madison,+WI";
+// var menuURL =
+//   "https://api.eatstreet.com/publicapi/v1/restaurant/90fd4587554469b1f15b4f2e73e761809f4b4bcca52eedca/menu?includeCustomizations=false";
+// var registerUrl = "https://api.eatstreet.com/publicapi/v1/register-user";
